49b7f1d81a7d6bc7a610e88f6ba3dfac49499f60
Govind9066 <gadekargovind575@gmail.com>
2025-09-23 15:23:42 +0530

    code in VS code 5 example of if-else statements

---
+++ sept-23.py
@@ -0,0 +1,41 @@
+print("Sept-23")
+
+print("5 Example of If-Else statement : ")
+# 1
+x=10
+y=5
+if x==y:
+    print("Same")
+else:
+    print("Not same")
+
+# 2
+age=15
+if age >= 18:
+    print("You are eligibal for lincence")
+else:
+    print("You are not eligibal for lincence")
+ 
+ # 3
+a=10
+if a % 2==0:
+    print(a," is even")
+else:
+    print(a," is odd")
+
+# 4
+a=5
+if a > 5:
+    print(a," is greate than 5")
+else:
+    print(a," is not greater than 5")
+
+# 5
+x=int(input("Enter a number : "))
+
+if x > 0:   
+    print(x," is positive number")
+elif x < 0:
+    print(x," is negative number")
+else:
+    print(x," is Zero")
