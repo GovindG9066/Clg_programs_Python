b7d5e0e0f19f45021d17a1c9b62ce98e62ec93c0
Govind9066 <gadekargovind575@gmail.com>
2025-09-23 15:53:46 +0530

    for loop program

--- sept-23.py
+++ sept-23.py
@@ -39,3 +39,47 @@ elif x < 0:
     print(x," is negative number")
 else:
     print(x," is Zero")
+
+print("for loop : ")
+
+# 1
+a=int(input("Enter a number : "))
+
+for i in range(a,a+11):
+    print(i)
+
+# 2 
+a=int(input("Enter a number : "))
+for i in range(a,a*10+1):
+    if i % a == 0:
+        print(i)
+
+# 3
+num=6
+
+for i in range(num,num*10+1,num):
+    print(i)
+
+# 4
+
+num=7
+isture=False
+
+for i in range(1,num):
+    if num % i != 0:
+        istrue=True
+    else:
+        istrue=False
+if istrue==True:
+    print(num," is prime")
+else:
+    print(num," not a prime number")
+
+# 5
+
+num=9
+for i in range(1,num+1):
+    if num % i==0:
+        print(i)
+
+
