df76019d990aebfdbd9935a8740c95700533fb58
Govind9066 <gadekargovind575@gmail.com>
2025-10-12 17:45:58 +0530

    Linked List traversal

--- Oct-12-DSA.py
+++ Oct-12-DSA.py
@@ -1,1 +1,34 @@
 print("DSA")
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
+    
+    def traversal(self,head):
+        current=head
+        while current:
+            print(current.data,end="->")
+            current=current.next
+        print("Null")
+
+
+n1=Node(25)
+n2=Node(50)
+n3=Node(75)
+n4=Node(100)
+
+n1.next=n2
+n2.next=n3
+n3.next=n4
+
+def main():
+    ll=LinkedList()
+    ll.traversal(n1)
+
+if __name__=="__main__":
+    main()
