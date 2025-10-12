469aee1bf5434608de5e679aac954f2b936645a9
Govind9066 <gadekargovind575@gmail.com>
2025-10-12 17:50:54 +0530

    insert at beginning

--- Oct-12-DSA.py
+++ Oct-12-DSA.py
@@ -16,6 +16,11 @@ class LinkedList:
             current=current.next
         print("Null")
 
+    def insert_at_beginning(self,new_data):
+        new_node=Node(new_data)
+        new_node.next=self.head
+        self.head=new_node
+
 
 n1=Node(25)
 n2=Node(50)
@@ -28,7 +33,9 @@ n3.next=n4
 
 def main():
     ll=LinkedList()
-    ll.traversal(n1)
+    ll.head=n1
+    ll.insert_at_beginning(101)
+    ll.traversal(ll.head)
 
 if __name__=="__main__":
     main()
