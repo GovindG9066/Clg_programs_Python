2d0b04d09b7d631899d735012ffd7131a6b0bb1a
Govind9066 <gadekargovind575@gmail.com>
2025-11-03 21:14:24 +0530

    singly queue

--- Nov/Nov-03.py
+++ Nov/Nov-03.py
@@ -1,2 +1,36 @@
 print("Nov-03")
 
+class queue:
+    def __init__(self):
+        self.items=[]
+
+    def is_empty(self):
+        return len(self.items)==0
+    
+    def push(self,item):
+        self.items.append(item)
+
+    def mypop(self):
+        if self.is_empty():
+            print("Queue is empty :")
+        else:
+            self.items.pop(0)
+
+    def display(self):
+        if self.is_empty():
+            print("Queue is empty")
+        else:
+            print(self.items)
+
+if __name__=="__main__":
+    q=queue()
+    q.push(10)
+    q.push(20)
+    q.push(30)
+    q.push(40)
+    
+    q.display()
+
+    q.mypop()
+
+    q.display()
