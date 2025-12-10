8ad05624b871eed9c1a9fab60fdff46c78cafa71
Govind9066 <gadekargovind575@gmail.com>
2025-12-11 00:20:40 +0530

    fib using recursion

---
+++ Dec/Dec-10.py
@@ -0,0 +1,12 @@
+print("Dec 10")
+print("Practical question :")
+#1
+
+def fib(first,second,n):
+    if n is 0:
+        return
+    sum=first+second
+    print(first)
+    fib(second,sum,n-1)
+
+fib(0,1,10)
