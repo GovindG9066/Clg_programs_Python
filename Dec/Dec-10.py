7ccb8b36bd3b14cfd53843bfd4c17925a4f3c5ad
Govind9066 <gadekargovind575@gmail.com>
2025-12-11 00:51:57 +0530

    sum of square of first n natural number

--- Dec/Dec-10.py
+++ Dec/Dec-10.py
@@ -13,7 +13,7 @@ fib(0,1,10)
 
 
 # 1 factorial of the num
-print("Factorial of the number :")
+print("1. Factorial of the number :")
 
 def facto(num):
     if num == 1:
@@ -22,3 +22,12 @@ def facto(num):
 
 print(facto(5))
 
+# 2 sum of square of first n natural num
+print("2. sum of square of first n natural number :")
+
+def sum_of_squares(num):
+    if num == 1:
+        return 1
+    return (num**2) + sum_of_squares(num-1)
+
+print(sum_of_squares(5))
