4aa931f2150f767fb4066ad775364e1e999cf2a1
Govind9066 <gadekargovind575@gmail.com>
2025-12-11 00:43:43 +0530

    factorial  of the number using recursion

--- Dec/Dec-10.py
+++ Dec/Dec-10.py
@@ -1,6 +1,6 @@
 print("Dec 10")
 print("Practical question :")
-#1
+#fib
 
 def fib(first,second,n):
     if n is 0:
@@ -10,3 +10,15 @@ def fib(first,second,n):
     fib(second,sum,n-1)
 
 fib(0,1,10)
+
+
+# 1 factorial of the num
+print("Factorial of the number :")
+
+def facto(num):
+    if num == 1:
+        return 1
+    return num*facto(num-1)
+
+print(facto(5))
+
